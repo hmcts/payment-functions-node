cd237f7a8b830f40d35f9905f5c94603bd342f2e
Sateesh Mathangi <a-smathangi@hotels.com>
2018-11-12 13:11:12 +0000

    reverted prev changes

--- uploadSource.sh
+++ uploadSource.sh
@@ -8,7 +8,7 @@ env=$4
 cd $zipDir
 #echo "Installing node modules and extensions"
 npm install
-dotnet build -o bin
+#dotnet build -o bin
 #func extensions install  # Jenkins need to install azure-functions-core-tools
 
 echo "Zipping function contents"

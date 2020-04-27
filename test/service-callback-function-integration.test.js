5caa4bbcca7b15d9274a80ea62a5a91c476322df
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-04-27 20:44:47 +0100

    Updating module location

--- test/service-callback-function-integration.test.js
+++ test/service-callback-function-integration.test.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const PORT = 4872;
 
-const serviceCallbackFunction = require('../ServiceCallbackFunction');
+const serviceCallbackFunction = require('../ServiceCallbackFunction/index.js');
 
 let context, bindingData;
 

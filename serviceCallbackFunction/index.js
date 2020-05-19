e658dea4ec9ef52411629ffb27f7f34b0febb97f
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-05-19 12:47:51 +0100

    Add appinsights

--- serviceCallbackFunction/index.js
+++ serviceCallbackFunction/index.js
@@ -1,4 +1,9 @@
 const serviceCallbackFunction = require('./serviceCallbackFunction');
+const appInsights = require("applicationinsights");
+const config = require('config');
+
+appInsights.setup(config.get('appInsightsInstumentationKey'));
+appInsights.start();
 serviceCallbackFunction().catch((err) => {
   console.log("Error occurred: ", err);
 });

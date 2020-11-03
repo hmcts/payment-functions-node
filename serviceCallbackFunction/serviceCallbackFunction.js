cc984fadde69c28021a7e052ae906e4eedef05e6
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-11-03 14:58:24 +0000

    PRConfig Issues

--- serviceCallbackFunction/serviceCallbackFunction.js
+++ serviceCallbackFunction/serviceCallbackFunction.js
@@ -71,29 +71,38 @@ module.exports = async function serviceCallbackFunction() {
                         },
                         json: true,
                         body: msg.body
-                    });
-                    if(serviceResponse && serviceResponse.status >= 200 && serviceResponse.status < 300) {
-                        console.log('Response : ' + JSON.parse(response));
-                        console.log('Message Sent Successfully to ' + serviceCallbackUrl);
-                    } else {
-                        console.log('Error in Calling Service ' + serviceResponse.status + ' response ' + serviceResponse.message);
-                        if (!msg.userProperties.retries) {
-                            msg.userProperties.retries = 0;
-                        }
-                        if (msg.userProperties.retries === MAX_RETRIES) {
-                            console.log("Max number of retries reached for ", JSON.stringify(msg.body));
-                            msg.deadLetter()
-                                .then(() => {
-                                    console.log("Dead lettered a message ", JSON.stringify(msg.body));
-                                })
-                                .catch(err => {
-                                    console.log("Error while dead letter message ", err)
-                                });
-                        } else {
-                            msg.userProperties.retries++;
-                            sendMessage(msg.clone());
-                        }
+                    }, function(response, error, body) {
+                    console.log('response-----444444 : ' + JSON.stringify(response) + JSON.stringify(error));
+                    if(response) {
+                    console.log('response-----444444 : ' + JSON.stringify(response));
                     }
+                    if(error) {
+                    console.log('error-----444444 : ' + JSON.stringify(error));
+                    }
+                    });
+//                    console.log('ServiceResponse-----444444 : ' + JSON.parse(serviceResponse));
+//                    if(serviceResponse && serviceResponse.status >= 200 && serviceResponse.status < 300) {
+//                        console.log('Response : ' + JSON.parse(response));
+//                        console.log('Message Sent Successfully to ' + serviceCallbackUrl);
+//                    } else {
+//                        console.log('Error in Calling Service ' + serviceResponse.status + ' response ' + serviceResponse.message);
+//                        if (!msg.userProperties.retries) {
+//                            msg.userProperties.retries = 0;
+//                        }
+//                        if (msg.userProperties.retries === MAX_RETRIES) {
+//                            console.log("Max number of retries reached for ", JSON.stringify(msg.body));
+//                            msg.deadLetter()
+//                                .then(() => {
+//                                    console.log("Dead lettered a message ", JSON.stringify(msg.body));
+//                                })
+//                                .catch(err => {
+//                                    console.log("Error while dead letter message ", err)
+//                                });
+//                        } else {
+//                            msg.userProperties.retries++;
+//                            sendMessage(msg.clone());
+//                        }
+//                    }
                 }).catch(error => {
                     console.log('Error in fetching S2S token message ' + error.message + ' response ' + error.response);
                 });
